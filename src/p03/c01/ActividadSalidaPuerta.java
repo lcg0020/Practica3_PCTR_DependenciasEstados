95f6c493a5710188598581ca20ebe1a9a91b20a7
lcg0020 <57801031+lcg0020@users.noreply.github.com>
2021-04-08 16:01:13 +0200

    Update ActividadSalidaPuerta.java

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,34 @@
 package p03.c01;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
 
-	//
-	// TODO
-	//
+		private static final int NUMENTRADAS = 20;
+		private String puerta;
+		private IParque parque;
+
+		public ActividadSalidaPuerta(String puerta, IParque parque) {
+			this.puerta = puerta;
+			this.parque = parque;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i < NUMENTRADAS; i ++) {
+				try {
+					parque.salirDelParque(puerta);
+					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+				} catch (InterruptedException e) {
+					Logger.getGlobal().log(Level.INFO, "Entrada interrumpida");
+					Logger.getGlobal().log(Level.INFO, e.toString());
+					return;
+				}
+			}
+		}
+
 }
+	
+
